dcac0307e86afe4f7bc2834563de95de18a00fa4
miloee <netelf05@outlook.com>
2018-02-13 18:29:22 +0800

    svm commit 1

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -35,13 +35,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,y[i]] -= X[i]
+        dW[:,j] += X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += 2 * reg * np.sum(W)
 
   #############################################################################
   # TODO:                                                                     #
@@ -63,8 +67,15 @@ def svm_loss_vectorized(W, X, y, reg):
   Inputs and outputs are the same as svm_loss_naive.
   """
   loss = 0.0
+  num_train = X.shape[0]
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
+  scores = X.dot(W)
+  correct_class_scores = scores[ y]
+  print(y.shape, correct_class_scores.shape)
+  print(scores,scores.shape)
+  print(scores[y],scores[y].shape)
+  margins = np.maximum(0, scores - scores[y] + 1)
+  loss = np.sum(margins) / num_train
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
